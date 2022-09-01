1a9ca55ce14ba5e4e14630ba7a15b710de1baf8b
Chayan Sarkar <59403799+apu-hub@users.noreply.github.com>
2022-09-01 09:50:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (<main>
+    Create the initial application and add it to GitHub.
+  </main>
+  );
+}
+export default App;
